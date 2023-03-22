4a6378267756242dc85fd647a0e42959ed745429
OceJlot <ocejlot@gmail.com>
2023-03-22 20:29:03 +0200

    f

--- src/main/java/net/ocejlot/lobbycfg/lobbycfg/listeners/OnBreak.kt
+++ src/main/java/net/ocejlot/lobbycfg/lobbycfg/listeners/OnBreak.kt
@@ -1,27 +1,38 @@
 package net.ocejlot.lobbycfg.lobbycfg.listeners
 
-import io.papermc.paper.event.block.BlockBreakBlockEvent
+import org.bukkit.Material
 import org.bukkit.configuration.Configuration
-import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 
 
-class BlockBreakListener(
+class OnBreak(
     private val config: Configuration
 ): Listener{
 
+    private val allowBlockBreak = config.getConfigurationSection("allowBlockBreak")
+    private val blockBreakingIsEnabled = allowBlockBreak!!.getBoolean("toggle")
+    private val blockBreakPerm = allowBlockBreak!!.getString("bypassPermission")
+    private val exceptions = allowBlockBreak!!.getStringList("exceptions")
+    private val allowedWorlds = config.getStringList("allowedWorlds")
+
     @EventHandler
-    fun breakBlockLogic(event: BlockBreakEvent){
-        val blockBreakingIsEnabled = config.getBoolean("disallowBlockBreak.toggle")
-        if(!blockBreakingIsEnabled)return
-
+    fun onBreak(event: BlockBreakEvent){
         val player = event.player
+        if(!allowedWorlds.contains(player.world.name))return
 
-        val blockBreakPerm = config.getString("disallowBlockBreak.bypassPermission").toString()
-        if(player.hasPermission(blockBreakPerm))return
+        val block = event.block
+        if(!blockBreakingIsEnabled)return
+        if(player.hasPermission(blockBreakPerm!!))return
+
+        val materialExceptions = arrayListOf<Material>()
+        exceptions.forEach{block->
+            val m = Material.getMaterial(block)
+            materialExceptions.add(m!!)
+        }
+        if(materialExceptions.contains(block.type))return
+
         event.isCancelled = true
-
     }
 }
--- src/main/java/net/ocejlot/lobbycfg/lobbycfg/listeners/OnPlace.kt
+++ src/main/java/net/ocejlot/lobbycfg/lobbycfg/listeners/OnPlace.kt
@@ -1,4 +1,38 @@
 package net.ocejlot.lobbycfg.lobbycfg.listeners
 
-class OnPlace {
+import org.bukkit.Material
+import org.bukkit.configuration.Configuration
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
+
+
+class OnPlace(
+    private val config: Configuration
+): Listener{
+
+    private val allowBlockPlace = config.getConfigurationSection("allowBlockPlace")
+    private val blockPlaceIsEnabled = allowBlockPlace!!.getBoolean("toggle")
+    private val blockPlacePerm = allowBlockPlace!!.getString("bypassPermission")
+    private val exceptions = allowBlockPlace!!.getStringList("exceptions")
+    private val allowedWorlds = config.getStringList("allowedWorlds")
+
+    @EventHandler
+    fun onPlace(event: BlockPlaceEvent){
+        val player = event.player
+        if(!allowedWorlds.contains(player.world.name))return
+
+        val block = event.block
+        if(!blockPlaceIsEnabled)return
+        if(player.hasPermission(blockPlacePerm!!))return
+
+        val materialExceptions = arrayListOf<Material>()
+        exceptions.forEach{block->
+            val m = Material.getMaterial(block)
+            materialExceptions.add(m!!)
+        }
+        if(materialExceptions.contains(block.type))return
+
+        event.isCancelled = true
+    }
 }
